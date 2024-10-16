14e0c32f9041b3a2ed3d7afcbf360f85d6b3671e
woon <sherman@mobilehead.com>
2024-10-16 05:36:22 -0600

    Update constants.jsx

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,6 +1,6 @@
 // export const serverUrl = `${process.env.VITE_SERVER_URL}/api`;
 // export const telegramId = `${process.env.VITE_TELEGRAM_ID}`;
 // export const username=`${process.env.VITE_USERNAME}`;
-export const serverUrl = `http://pvptest.wostore.xyz/api`;
+export const serverUrl = `https://pvptest.wostore.xyz/api`;
 export const telegramId = `123123`;
 export const username=`Winner`;
