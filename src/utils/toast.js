42b62d280784f5dd70d06bd634891ba5d0857c56
RichDash <Rich_Dash0128@outlook.com>
2024-10-23 17:11:10 -0700

    update toast time

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -3,7 +3,7 @@ import { toast, Slide } from "react-toastify";
 export default (type, message) => {
   toast[type](message, {
     position: "top-center",
-    autoClose: 25000,
+    autoClose: 2500,
     hideProgressBar: true,
     transition: Slide,
   });
