540fcab8f9ba11627aa6888dea5bae848ebab3c5
RichDash <Rich_Dash0128@outlook.com>
2024-10-25 13:06:37 -0700

    update server url

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,5 @@
-export const baseUrl = "http://172.20.30.99:5002";
-// export const baseUrl = "https://pvptest.wostore.xyz";
+// export const baseUrl = "http://172.20.30.99:5002";
+export const baseUrl = "https://pvptest.wostore.xyz";
 // export const baseUrl = "http://localhost:5002";
 export const serverUrl = `${baseUrl}/api`;
 export const telegramId = "123123";
